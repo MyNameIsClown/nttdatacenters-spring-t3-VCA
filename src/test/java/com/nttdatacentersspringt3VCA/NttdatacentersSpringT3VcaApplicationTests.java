6c7f581c87d6f033331ba200ea98a6fd54f43e94
MyNameIsClown <96545817+MyNameIsClown@users.noreply.github.com>
2022-12-14 09:37:07 +0100

    Final Implementation

---
+++ src/test/java/com/nttdatacentersspringt3VCA/NttdatacentersSpringT3VcaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdatacentersspringt3VCA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdatacentersSpringT3VcaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
